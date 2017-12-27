6522f849fd9947f0ce4deec4d8473d09e9a0d98e
ijrys <hanqindong@outlook.com>
2017-12-27 19:03:13 +0800

    cteate

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Demo {
 	class Program {
+		static void DoFun (object oname) {
+			string name = (string) oname;
+			string connstr = "server=172.16.1.100,2356;uid=sa;pwd=1234567890hh;database=SQLDemo";
+			SqlConnection conn = new SqlConnection(connstr);
+			using (SqlCommand cmd = conn.CreateCommand()) {
+				cmd.CommandText = $"EXECUTE [dbo].[Demo] @pname = N'{name}'";
+				conn.Open();
+				string restr = cmd.ExecuteScalar().ToString();
+				Console.WriteLine(restr + " " + name);
+			}
+		}
+
 		static void Main(string[] args) {
+			Thread th1 = new Thread(new ParameterizedThreadStart(DoFun));
+			Thread th2 = new Thread(new ParameterizedThreadStart(DoFun));
+			th1.Start("张三");
+			Thread.Sleep(20);
+			th2.Start("李四");
+
+
+			Console.ReadLine();
 		}
 	}
 }
